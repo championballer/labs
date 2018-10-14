c66ec5060e8adad77b14b2059d74492ab4ddcbef
Shiv Kumar <championballer08@gmail.com>
2018-10-14 18:59:11 +0530

    added basic echo underlines

--- linux/.hello.sh
+++ linux/.hello.sh
@@ -2,4 +2,12 @@
 
 #comment
 
-echo "Hello world"
+echo Hello 				World #Parameters are single spaced by default
+echo "Hello 	world" #Using quotes allows us to add appropriate spaces wherever needed
+echo *
+echo Hello * World
+
+# * is to list the files in the current directory, and won't show the hidden(.) files
+
+echo `echo Hello` world
+
