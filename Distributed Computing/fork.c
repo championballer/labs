5568b1624eed906761d182872480b238f47d35b3
championballer <championballer08@gmail.com>
2020-01-07 17:30:24 +0530

    Added fork process code

--- Distributed Computing/fork.c
+++ Distributed Computing/fork.c
@@ -8,8 +8,34 @@ int main()
   
     // make two process which run same 
     // program after this instruction 
-    fork(); 
-  
-    printf("Hello world!\n"); 
-    return 0; 
+    int x = 1;
+    int ret = fork();
+
+    //positive value - parent process 
+    //zero value - child process
+  	
+
+  	pid_t pid = getpid();
+
+  	printf("pid: %lun \n", pid);
+
+
+  	if(ret==0)
+  	{
+  		printf("child process\n");
+    	printf("Hello world!\n");
+    	printf("%d\n",++x);	
+  	}
+
+  	else 
+  	{
+  		printf("process id of created child process is %d\n",ret);
+  		printf("parent process\n");
+    	printf("Hello world!\n");
+    	printf("%d\n",--x);
+  	 
+     
+  	}
+
+  	return 0;
 } 
