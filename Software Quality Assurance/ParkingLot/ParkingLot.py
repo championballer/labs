d38967c0b4338db6f8e5950044434c957b8246b7
Shiv Kumar <championballer08@gmail.com>
2020-04-18 06:52:33 +0530

    Update repo

---
+++ Software Quality Assurance/ParkingLot/ParkingLot.py
@@ -0,0 +1,54 @@
+"""
+Contains code for Parking Lot file 
+"""
+
+class ParkingLot:
+	#Sample comment
+	pass
+
+class ParkingFloor:
+	pass
+
+class ParkingSpot:
+	pass
+
+class Account:
+	pass
+
+class ParkingTicket:
+	pass
+
+class Vehicle:
+	pass
+
+class Car(Vehicle):
+	pass
+
+class Truck(Vehicle):
+	pass
+
+class MotorCycle(Vehicle):
+	pass
+
+class EntrancePanel:
+	pass
+
+class ExitPanel:
+	pass
+
+class Payment:
+	pass
+
+class ParkingDisplayBoard:
+	pass
+
+class ParkingAttendantPortal:
+	pass
+
+class CustomerInfoPanel:
+	pass
+
+class ElectricPanel:
+	pass
+
+
