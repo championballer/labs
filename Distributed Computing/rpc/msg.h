54c4bf5340141901b24ee6c199ba1a7d59c7663a
championballer <championballer08@gmail.com>
2020-04-24 19:05:09 +0530

    Added output and other support files for RPC

--- Distributed Computing/rpc/msg.h
+++ Distributed Computing/rpc/msg.h
@@ -6,28 +6,32 @@
 #ifndef _MSG_H_RPCGEN
 #define _MSG_H_RPCGEN
 
-#define RPCGEN_VERSION	199506
-
 #include <rpc/rpc.h>
 
 
-#define MESSAGEPROG ((rpc_uint)0x20000001)
-#define PRINTMESSAGEVERS ((rpc_uint)1)
-
 #ifdef __cplusplus
-#define PRINTMESSAGE ((rpc_uint)1)
-extern "C" int * printmessage_1(char **, CLIENT *);
-extern "C" int * printmessage_1_svc(char **, struct svc_req *);
+extern "C" {
+#endif
 
-#elif __STDC__
-#define PRINTMESSAGE ((rpc_uint)1)
+
+#define MESSAGEPROG 0x20000001
+#define PRINTMESSAGEVERS 1
+
+#if defined(__STDC__) || defined(__cplusplus)
+#define PRINTMESSAGE 1
 extern  int * printmessage_1(char **, CLIENT *);
 extern  int * printmessage_1_svc(char **, struct svc_req *);
+extern int messageprog_1_freeresult (SVCXPRT *, xdrproc_t, caddr_t);
 
-#else /* Old Style C */
-#define PRINTMESSAGE ((rpc_uint)1)
+#else /* K&R C */
+#define PRINTMESSAGE 1
 extern  int * printmessage_1();
 extern  int * printmessage_1_svc();
-#endif /* Old Style C */
+extern int messageprog_1_freeresult ();
+#endif /* K&R C */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !_MSG_H_RPCGEN */
